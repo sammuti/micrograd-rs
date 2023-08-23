bf1664757fc633d61c68ea48e4a35e481330a104
Vim Wickramasinghe <vim@sammuti.com>
2023-08-23 13:45:19 +0200

    Backprop works with mul and add

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// Trying to build a rust based version of micrograd deep learning framework
+// Trying to build a rust based version of micrograd https://github.com/karpathy/micrograd
 use std::rc::Rc;
 use std::cell::RefCell;
 use Config::EdgeNoLabel;
@@ -34,7 +34,9 @@ impl Value {
                 self.prev[0].borrow_mut().grad += self.grad * self.prev[1].borrow().data;
                 self.prev[1].borrow_mut().grad += self.grad * self.prev[0].borrow().data;
             }
-            _ => {}
+            _ => {
+
+            }
         }
     }
 }
@@ -68,10 +70,10 @@ fn build_nodes_and_edges(root: Rc<RefCell<Value>>) -> DiGraph<String, ()>{
 }
 
 fn build_graph(node: Rc<RefCell<Value>>, d: &mut DiGraph<String, ()>) -> Option<NodeIndex> {
-    let node_label = format!("{} | data={}",
+    let node_label = format!("{} | data={} |  grad={}",
                              node.borrow().clone().label
                                  .unwrap_or("--".to_string()).clone(),
-                             node.borrow().clone().data);
+                             node.borrow().clone().data, node.borrow().clone().grad);
     // Bug: When there are multiple ops from a single input this only visualizes the first one
     if d.node_weights().all(|w| *w != node_label) {
         let nix = d.add_node(node_label.clone());
@@ -94,16 +96,80 @@ fn build_graph(node: Rc<RefCell<Value>>, d: &mut DiGraph<String, ()>) -> Option<
     None
 }
 
+fn backward(root: Rc<RefCell<Value>>) {
+    let mut visited = vec![];
+    let mut stack = vec![];
+    root.borrow_mut().grad = 1.0;
+    stack.push(root);
+    while stack.len() > 0 {
+        let node = stack.pop().unwrap();
+        if !visited.contains(&node) {
+            visited.push(node.clone());
+            for child in node.borrow().clone().prev {
+                stack.push(child);
+            }
+        }
+    }
+    for node in visited.iter() {
+        node.borrow_mut().backward();
+    }
+}
+
 fn main() {
+    sample2();
+}
+
+fn sample2() {
     let a = Rc::new(RefCell::new(Value {
-        data: 1.1,
+        data: 2.0,
         grad: 0.0,
         prev: vec![],
         op: Op::None,
         label: Some("a".to_string())
     }));
     let b = Rc::new(RefCell::new(Value {
+        data: -3.0,
+        grad: 0.0,
+        prev: vec![],
+        op: Op::None,
+        label: Some("b".to_string())
+    }));
+    let c = Rc::new(RefCell::new(Value {
+        data: 10.0,
+        grad: 0.0,
+        prev: vec![],
+        op: Op::None,
+        label: Some("c".to_string())
+    }));
+    let f= Rc::new(RefCell::new(Value {
+        data: -2.0,
+        grad: 0.0,
+        prev: vec![],
+        op: Op::None,
+        label: Some("f".to_string())
+    }));
+    let e = mul(a.clone(), b.clone());
+    e.borrow_mut().label = Some("e".to_string());
+    let d = add(e.clone(), c.clone());
+    d.borrow_mut().label = Some("d".to_string());
+    let l = mul(d.clone(), f.clone());
+    l.borrow_mut().label = Some("l".to_string());
+
+    backward(l.clone());
+    let di = build_nodes_and_edges(l);
+    println!("{:?}", Dot::with_config(&di, &[EdgeNoLabel]));
+}
+
+fn sample1() {
+    let a = Rc::new(RefCell::new(Value {
         data: 2.0,
+        grad: 0.0,
+        prev: vec![],
+        op: Op::None,
+        label: Some("a".to_string())
+    }));
+    let b = Rc::new(RefCell::new(Value {
+        data: -3.0,
         grad: 0.0,
         prev: vec![],
         op: Op::None,
@@ -115,6 +181,8 @@ fn main() {
     d.borrow_mut().label = Some("d".to_string());
     let e = mul(c.clone(), d.clone());
     e.borrow_mut().label = Some("e".to_string());
+
+    backward(e.clone());
     let di = build_nodes_and_edges(e);
     println!("{:?}", Dot::with_config(&di, &[EdgeNoLabel]));
 }
